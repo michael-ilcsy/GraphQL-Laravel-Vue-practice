77fdc94941f9bae2a531f848411556aa3be724e3
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-06-05 19:17:40 +0900

    feat: vueのプロジェクト作成
    p70～79

---
+++ twitter-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Twitter Like App</span>
+        <span class="font-weight-light"> | sample App</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
