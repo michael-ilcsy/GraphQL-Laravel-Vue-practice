2f5d7dcb58573cdfbb03a98f50b2cb357bbc84ee
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-06-05 19:52:49 +0900

    feat: apolloの設定
    p80～82

--- twitter-frontend/src/main.js
+++ twitter-frontend/src/main.js
@@ -4,10 +4,47 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { ApolloClient } from 'apollo-client'
+import { setContext } from 'apollo-link-context'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
 Vue.config.productionTip = false
+
+Vue.use(VueApollo)
+// createHttpLinkでエンドポイントの指定をします
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql'
+})
+
+// 今回はlocalstorageを用いてtokenを保持します
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('vue_token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+// Apolloclientの初期化です
+// linkとmemoryCacheを有効にします
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+// apolloProviderを設定します。
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  errorHandler (error) {
+    store.commit('error')
+  }
+})
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
